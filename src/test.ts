a58d837f796ad253c6ef13f5306da2daa6900ecf
Marcos Essindi <marcessindi@me.com>
2026-01-12 18:09:09 +0100

    Test sdk

---
+++ src/test.ts
@@ -0,0 +1,10 @@
+import { sdk } from "./index.js";
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+process.env.WORKFLOW_APP_HOST = "http://localhost:3000";
+
+async function main() {
+	const lala = await sdk.hello({ words: ["hello", "world"] });
+	console.dir(lala, { depth: Infinity });
+}
+
+main().catch(console.error);
