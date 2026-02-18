9f754070bd205f8484cedb004dc1cc831d2a106e
Marcos Essindi <marcessindi@me.com>
2026-02-18 13:53:28 +0100

    chore(sdk): prepare for extraction to standalone repo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,32 @@
-import { config as baseConfig } from "@repo/eslint-config/base";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
 
-export default baseConfig;
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config}
+ * */
+export default [
+	js.configs.recommended,
+	eslintConfigPrettier,
+	...tseslint.configs.recommended,
+	{
+		plugins: {
+			turbo: turboPlugin,
+		},
+		rules: {
+			"turbo/no-undeclared-env-vars": "warn",
+		},
+	},
+	{
+		plugins: {
+			onlyWarn,
+		},
+	},
+	{
+		ignores: ["dist/**"],
+	},
+];
